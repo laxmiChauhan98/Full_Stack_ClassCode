4e4083c1b427572358180270b675f2b1be7fdefd
laxmiChauhan98 <laxchauhan98@gmail.com>
2026-02-09 10:34:59 +0530

    09-02-2026

--- backend/osModule.js
+++ backend/osModule.js
@@ -1,19 +1,15 @@
 const os = require("os");
 
 
-
-
-
-
-
-// console.log("OS Platform:", os.platform());
-// console.log("CPU Architecture:", os.arch());
-// console.log(os.cpus());
-// console.log("CPU Cores:", os.cpus().length);
-// console.log("Free Memory:", os.freemem());
-// console.log("Total Memory:", os.totalmem());
-// console.log(os.hostname());
-// console.log(os.homedir());
-// console.log(os.tmpdir());
+console.log("OS Platform:", os.platform());
+console.log("CPU Architecture:", os.arch());
+console.log(os.cpus());
+console.log("CPU Cores:", os.cpus().length);
+console.log("Free Memory:", os.freemem());
+console.log("Total Memory:", os.totalmem());
+console.log(os.hostname());
+console.log(os.homedir());
+console.log(os.tmpdir());
+console.log(os.uptime());
 
 
--- backend/pathModule.js
+++ backend/pathModule.js
@@ -1,19 +1,17 @@
 const path = require("path");
 
-const filePath1 = path.join(__dirname, "abcd.txt");
-console.log(filePath1);
-
-
-const filePath = path.join(
-  __dirname,
-  "..",
-  "..",
-  "file",
-  "abcd.txt"
-);
-
+const filePath = path.join(__dirname,"files","abcd.txt");  //joins the path segments
 console.log(filePath);
+
+// const filePath = path.join(
+//   __dirname,
+//   "..",
+//   "..",
+//   "file",
+//   "abcd.txt"
+// );
+//console.log(filePath);
 
 console.log(path.basename(filePath));
 console.log(path.extname("employee.pdf"));
-console.log(path.resolve("files", "data.txt"));
+console.log(path.resolve("files", "data.txt")); // genrates absolute path
